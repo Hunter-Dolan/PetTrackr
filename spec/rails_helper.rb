c2526502cab9706744d6bffb1378dc06d2593ea2
Hunter Dolan <hunterhdolan@gmail.com>
2018-10-06 11:43:55 -0400

    Configure travis for chrome and yarn

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,11 +9,13 @@ require "capybara/rspec"
 # Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
+
 Capybara.register_driver :selenium_chrome do |app|
  Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
 Capybara.javascript_driver = :selenium_chrome
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
